1df69c48c36e1ebd5f1df1808356137988b78af5
Vernon <me@example.com>
2017-04-18 15:40:55 -0500

    building appointment component

--- src/components/Appointments/addAppointment.js
+++ src/components/Appointments/addAppointment.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Panel, InputGroup, Form, Grid, Col, Row, FormControl, FormGroup, HelpBlock, ControlLabel} from 'react-bootstrap'
+import {Button, Checkbox, Table, Panel, InputGroup, Form, Grid, Col, Row, FormControl, FormGroup, HelpBlock, ControlLabel} from 'react-bootstrap'
 import './apptmnt.css'
 class AddAppointment extends Component {
   constructor(props) {
@@ -25,6 +25,7 @@ class AddAppointment extends Component {
 
     const topHeading = {
       marginBottom: '0px',
+      textAlign: 'left'
     }
 
     const headingMargin = {
@@ -42,9 +43,11 @@ class AddAppointment extends Component {
       <h3>Summary</h3>
     )
 
-    const titleSt = {
-      backgroundColor: '#00AD9A',
-      color: 'white'
+
+    const totalSt = {
+        fontWeight: 'bold',
+        fontSize: '1.25em',
+        marginTop: '10px'
     }
 
     return (
@@ -56,21 +59,21 @@ class AddAppointment extends Component {
 
           <Col sm={8}>
           <Form horizontal>
-          <h2 style={headingMargin}>Client Info</h2>
-            <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
+          <h2 style={headingMargin}>Who</h2>
+            <FormGroup >
+            <Col componentClass={ControlLabel} sm={3}>
                Email
              </Col>
-             <Col sm={10}>
+             <Col sm={9}>
                <FormControl type="email" placeholder="Email" />
              </Col>
             </FormGroup>
 
-            <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
+            <FormGroup >
+            <Col componentClass={ControlLabel} sm={3}>
                Name
              </Col>
-             <Col sm={5}>
+             <Col sm={4}>
                <FormControl type="text" placeholder="First Name" />
              </Col>
              <Col sm={5}>
@@ -78,26 +81,107 @@ class AddAppointment extends Component {
              </Col>
             </FormGroup>
 
-            <h2 style={headingMargin}>Appointment Service</h2>
+            <h2 style={headingMargin}>Where</h2>
+
+            <FormGroup >
+            <Col componentClass={ControlLabel} sm={3}>
+               Address
+             </Col>
+             <Col sm={9}>
+               <FormControl type="text" placeholder="Street" />
+             </Col>
+            </FormGroup>
+
+            <FormGroup >
+            <Col componentClass={ControlLabel} sm={3}>
+
+             </Col>
+             <Col sm={4}>
+               <FormControl type="text" placeholder="City" />
+             </Col>
+
+             <Col sm={2}>
+
+               <FormControl componentClass="select" placeholder="ST">
+                <option value="ST"></option>
+                 <option value="Arkansas">AK</option>
+                 <option value="Alaska">AL</option>
+                 <option value="Alaska">AR</option>
+                 <option value="Alaska">AL</option>
+                 <option value="Alaska">CA</option>
+                 <option value="Alaska">CO</option>
+                 <option value="Alaska">CT</option>
+                 <option value="Alaska">DC</option>
+                 <option value="Alaska">DE</option>
+                 <option value="Arizona">FL</option>
+               </FormControl>
+             </Col>
+             <Col sm={3}>
+               <FormControl type="text" placeholder="Zip" />
+             </Col>
+            </FormGroup>
+
+            <h2 style={headingMargin}>What</h2>
 
             <FormGroup controlId="formControlsSelect">
-
-              <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Type of Service</ControlLabel>
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Service</ControlLabel>
               </Col>
-              <Col sm={10}>
-              <FormControl componentClass="select" placeholder="select">
-                <option value="WebAppDev">Web App Development</option>
-                <option value="socialMedia">Social Media</option>
-                <option value="consulting">Consulting</option>
-                <option value="simpleWebsite">Simple Website</option>
-              </FormControl>
+
+              <Col sm={7}>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="WebAppDev">Web App Development</option>
+                  <option value="socialMedia">Social Media</option>
+                  <option value="consulting">Consulting</option>
+                  <option value="simpleWebsite">Simple Website</option>
+                </FormControl>
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formControlsSelect">
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Frequency</ControlLabel>
+              </Col>
+
+              <Col sm={7}>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="valueHere">One Time</option>
+                  <option value="valueHere">Monthly</option>
+                  <option value="valueHere">Weekly</option>
+                  <option value="valueHere">Biweekly</option>
+                </FormControl>
               </Col>
             </FormGroup>
 
             <FormGroup>
-              <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Final Price</ControlLabel>
+
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Extras</ControlLabel>
+              </Col>
+
+
+              <Col className="extras" sm={7}>
+                <Checkbox>
+                  Extra Item 1
+                </Checkbox>
+                <Checkbox>
+                  Extra Item 2
+                </Checkbox>
+                <Checkbox>
+                  Extra Item 3
+                </Checkbox>
+                <Checkbox>
+                  Extra Item 4
+                </Checkbox>
+                <Checkbox>
+                  Extra Item 5
+                </Checkbox>
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Tips</ControlLabel>
               </Col>
               <Col sm={5}>
                 <InputGroup>
@@ -109,18 +193,78 @@ class AddAppointment extends Component {
             </FormGroup>
 
 
+            <FormGroup>
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Final Price</ControlLabel>
+              </Col>
+              <Col sm={5}>
+                <InputGroup>
+                  <InputGroup.Addon>$</InputGroup.Addon>
+                  <FormControl type="text" />
+                  <InputGroup.Addon>.00</InputGroup.Addon>
+                </InputGroup>
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formControlsSelect">
+              <Col componentClass={ControlLabel} sm={3}>
+                <ControlLabel>Payment Method</ControlLabel>
+              </Col>
+
+              <Col sm={7}>
+                <FormControl componentClass="select" placeholder="ST">
+                  <option value="Cash">Cash</option>
+                  <option value="Check">Check</option>
+                  <option value="Online">Online</option>
+                  </FormControl>
+              </Col>
+            </FormGroup>
+
           </Form>
         </Col>
         <Col className="summary-col" sm={4}>
           <Panel header={title} style={blueBg}>
 
-            <p>Service</p>
-            <p>Extras</p>
-            <p>Discount</p>
-            <p>Adjustment</p>
-            <p>Tip</p>
-            <p>Total</p>
+            <Table responsive>
+              <tr>
+                <td>Service</td>
+                <td>$</td>
+              </tr>
+
+
+
+              <tr>
+                <td>Extras</td>
+                <td>$</td>
+              </tr>
+
+              <tr>
+                <td>Discount</td>
+                <td>$</td>
+              </tr>
+
+              <tr>
+                <td>Adjustment</td>
+                <td>$</td>
+              </tr>
+
+              <tr>
+                <td>Tip</td>
+                <td>$</td>
+              </tr>
+
+              <tr>
+                <td style={totalSt}>Total</td>
+                <td style={totalSt}>$</td>
+              </tr>
+
+
+
+            </Table>
           </Panel>
+
+          <Button className="panel-underbtn" bsSize="large" block>Save Changes</Button>
+
 
 
         </Col>
