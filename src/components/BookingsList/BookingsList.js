66d3d80b6709338572cfa69041beb67ab14ce0ec
daanishnasir <daanishnasir@gmail.com>
2017-04-20 11:26:58 -0500

    switch

--- src/components/BookingsList/BookingsList.js
+++ src/components/BookingsList/BookingsList.js
@@ -1,32 +1,13 @@
-
+import React from 'react';
 
 
 class MyComponent extends React.Component {
-  state = {
-    selectedDay: today(),
-  }
-  constructor(props) {
-    super(props);
-    this.handleDayClick = this.handleDayClick.bind(this);
-  }
-  handleDayClick(day, { disabled, selected }) {
-    if (disabled) {
-      return;
-    }
-    this.setState({
-      selectedDay: selected ? null : day
-    })
-  }
   render() {
     return (
-      <DayPicker
-        initialMonth={ new Date() }
-        disabledDays={ sundays }
-        selectedDays={ this.state.selectedDay }
-        onDayClick={ this.handleDayClick }
-        />);
+
+      );
   }
 }
 
 
-export default MyComponent;
+export default BookingsList;
