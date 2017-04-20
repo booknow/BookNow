94cd8c20700a42961efce77bb9ba51bb7b206b1c
Vernon <me@example.com>
2017-04-20 10:06:50 -0500

    removed empty nav list items

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css';
 
 
 export default class NavComponent extends Component {
-  
+
   render(){
       return (
           <Navbar inverse collapseOnSelect>
@@ -16,15 +16,7 @@ export default class NavComponent extends Component {
             </Navbar.Header>
             <Navbar.Collapse>
               <Nav>
-                <NavItem eventKey={1} href="#">Link</NavItem>
-                <NavItem eventKey={2} href="#">Link</NavItem>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                  <MenuItem eventKey={3.1}>Action</MenuItem>
-                  <MenuItem eventKey={3.2}>Another action</MenuItem>
-                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
+              
               </Nav>
               <Nav pullRight>
                 <NavDropdown eventKey={3} title="Help" id="basic-nav-dropdown">
