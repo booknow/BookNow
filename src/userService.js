0f7e6824f0a3b2e26c21660ca610ff39907e6c45
Qais Malik <qais.gh11@gmail.com>
2017-04-21 13:57:00 -0500

    hiding user's name on the login view

--- src/userService.js
+++ src/userService.js
@@ -5,7 +5,6 @@ const base_url = "/getCurrentUser";
 export function getUser() {
     const userPromise = axios.get("/getCurrentUser")
         .then(response => {
-            console.log(response);
             return response;
           })
     return userPromise;
