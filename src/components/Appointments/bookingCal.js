0064e9b2776f89708f7be86993f0f76dee935a6a
Vernon <me@example.com>
2017-04-25 09:46:36 -0500

    merging

--- src/components/Appointments/bookingCal.js
+++ src/components/Appointments/bookingCal.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
-import myEventsList from '../../utils/events';
+import axios from 'axios';
+// import myEventsList from '../../utils/events';
 import moment from 'moment';
+import API_BASE_URL from '../../utils/api-helper';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 // this weird syntax is just a shorthand way of specifying loaders
 // require('style!css!react-big-calendar/lib/css/react-big-calendar.css')
@@ -15,9 +17,44 @@ class MyCalendar extends Component {
 
   constructor () {
    super()
+
+
+
+
   }
 
+
+
+
   render() {
+
+    let tempArr = []
+
+    const prefs = axios.get(API_BASE_URL + '/api/setuppref')
+    .then(function(response) {
+      console.log(response);
+
+      let schedPrefs = response.data[0]
+      var today = new Date()
+      var todaysDay = today.getDay()
+
+
+      for (var prop in schedPrefs) {
+        if (schedPrefs[prop] === 'false') {
+          tempArr.push({
+            'title': 'Unavailable',
+            'allDay': true,
+            'start': new Date(),
+            'end': new Date()
+          })
+
+        }
+
+      }
+      console.log(tempArr);
+    })
+
+
     console.log(new Date);
     return (
       <div>
@@ -27,7 +64,7 @@ class MyCalendar extends Component {
         endAccessor='end'
         style={{height: '420px'}}
         timeslots={8}
-        events={myEventsList}
+        events={tempArr}
         />
       </div>
     )
