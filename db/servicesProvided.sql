7ad72720107bdc62fa1a08589880ca573c626c32
daanishnasir <daanishnasir@gmail.com>
2017-04-27 10:55:46 -0500

    facebook info

--- db/servicesProvided.sql
+++ db/servicesProvided.sql
@@ -1,3 +1,3 @@
-Select service_name, services_provided_price FROM services
+Select service_name, services_provided_price, sprovider_id FROM services
 JOIN services_provided
 ON services.service_id = services_provided.service_id
